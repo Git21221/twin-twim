7a782ba6ac14947cfa753e3dab8e8a18c51bc32f
Saikat Das <saikatdas40g@gmail.com>
2024-12-21 20:01:17 +0530

    feat: add logging for socket availability in Home component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -29,6 +29,7 @@ const Home = () => {
       console.log("Socket not available");
       return;
     }
+    console.log("Socket available");
     socket.emit("hii", "Hello from client");
     console.log(socket);
     socket?.on("connected", () => {
